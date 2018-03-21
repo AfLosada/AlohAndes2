d3060e720f09459ebe0e0c57145347b379247f4d
Aflosada <af.losada@uniandes.edu.co>
2018-03-21 00:45:04 -0500

    Pre 100%

--- src/rest/ApartamentoService.java
+++ src/rest/ApartamentoService.java
@@ -17,7 +17,6 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import tm.AlohAndesTransactionManager;
-import tm.ParranderosTransactionManager;
 import vos.Apartamento;
 import vos.Operador;
 
